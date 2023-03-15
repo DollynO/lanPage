469bd623388d31c03a412efbfdc87ddd2065a805
Johannes Obels <obels.johannes@web.de>
2023-03-15 22:51:59 +0100

    change logo to one component. add missing components for party.

---
+++ app/Http/Livewire/NewParty.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Party;
+use Livewire\Component;
+
+class NewParty extends Component
+{
+    public $start_date;
+    public $end_date;
+    public string $location;
+
+    protected $rules = [
+        'start_date' => 'required|date',
+        'end_date' => 'required|date|after:startDate',
+        'location' => 'required|string'
+    ];
+
+    public function render()
+    {
+        return view('livewire.new-party');
+    }
+
+    public function submit()
+    {
+        $validatedData = $this->validate();
+        $party = new Party();
+        $party->fill($validatedData);
+        $party->save();
+        redirect()->to('/parties');
+    }
+}
