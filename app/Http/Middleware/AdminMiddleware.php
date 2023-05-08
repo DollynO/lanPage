01e6bfb8637ec881aeaeb31d1eacc356a7192c37
Johannes Obels <obels.johannes@web.de>
2023-05-08 18:23:54 +0200

    create admin middleware to secure routes.

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user() && $request->user()->name === 'admin') {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
+    }
+}
