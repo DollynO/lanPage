b7f5774dd1df9f8dee7ecb1840ba43f4e293a4f4
ap <andreaspfanzelt@web.de>
2023-05-12 20:59:00 +0200

    Add tournament-leaderboard and tournament-game-suggestions to dashboard

---
+++ app/Http/Livewire/DashboardComponent/OverviewTournament.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\DashboardComponent;
+
+use Livewire\Component;
+
+class OverviewTournament extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard-component.overview-tournament');
+    }
+}
