e37b6cca3a0f78ed2b808f26541a718aa2b98a68
ap <andreaspfanzelt@web.de>
2023-05-13 11:24:23 +0200

    fix game-suggestions-tournament button css.

--- resources/views/livewire/game-suggestions-tournament.blade.php
+++ resources/views/livewire/game-suggestions-tournament.blade.php
@@ -61,25 +61,25 @@
                     <tbody class="text-gray-600 text-sm font-normal">
                         @forelse ($suggestions as $index => $suggestion)
                             <tr class="border-b border-gray-200 hover:bg-gray-100">
-                                <td class="px-3 py-2">{{ $suggestion['game']->name }}</td>
-                                <td class="px-3 py-2">{{ $suggestion->votes() }}</td>
-                                <td class="px-3 py-2">
+                                <x-td>{{ $suggestion['game']->name }}</x-td>
+                                <x-td>{{ $suggestion->votes() }}</x-td>
+                                <x-td>
                                     <button wire:click="increaseVotes( {{ $suggestion->id }} )"
-                                            class="bg-blue-500 text-white font-bold py-1 px-3 rounded"
+                                            class="outline-none inline-flex justify-center items-center group transition-all ease-in duration-150 focus:ring-2 focus:ring-offset-2 hover:shadow-sm disabled:opacity-80 disabled:cursor-not-allowed rounded gap-x-2 text-sm px-4 py-2     ring-negative-500 text-white bg-blue-500 hover:bg-blue-600 hover:ring-blue-600"
                                             @if ($suggestion->userVote(Auth::id()) || $suggestionsLeft == 0) style="display: none" @endif>
                                         +1
                                     </button>
                                     <button wire:click="decreaseVotes({{ $suggestion }})"
-                                            class="bg-red-500 text-white font-bold py-1 px-3 rounded"
+                                            class="outline-none inline-flex justify-center items-center group transition-all ease-in duration-150 focus:ring-2 focus:ring-offset-2 hover:shadow-sm disabled:opacity-80 disabled:cursor-not-allowed rounded gap-x-2 text-sm px-4 py-2     ring-negative-500 text-white bg-negative-500 hover:bg-negative-600 hover:ring-negative-600"
                                             @if (!$suggestion->userVote(Auth::id()) || $suggestion->votes() === 1) style="display: none" @endif>
                                         -1
                                     </button>
                                     <button wire:click="removeSuggestion({{ $suggestion->id }})"
-                                            class="bg-red-500 text-white font-bold py-1 px-3 rounded"
+                                            class="outline-none inline-flex justify-center items-center group transition-all ease-in duration-150 focus:ring-2 focus:ring-offset-2 hover:shadow-sm disabled:opacity-80 disabled:cursor-not-allowed rounded gap-x-2 text-sm px-4 py-2     ring-negative-500 text-white bg-negative-500 hover:bg-negative-600 hover:ring-negative-600"
                                             @if (!$suggestion->userVote(Auth::id()) || $suggestion->votes() > 1) style="display: none" @endif>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
                                     </button>
-                                </td>
+                                </x-td>
                             </tr>
                         @empty
                             <tr>
