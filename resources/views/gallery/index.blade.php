85a0fc555f5172f1a34bbabf969099ca2f697a0e
Andreas Pfanzelt <andreaspfanzelt@web.de>
2025-08-23 20:31:38 +0200

    fix gallery pop-up buttons and backdrop with darkmode

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -40,48 +40,67 @@
                         <div class="flex-shrink-0" style="width:160px; height:100px;">
                             <a href="{{ route('gallery.party', $party) }}"
                                class="w-full h-full flex flex-col items-center justify-center rounded border border-dashed border-gray-300 text-xs text-gray-700 font-semibold hover:bg-gray-50 transition">
-                                <div class="mb-1">+{{ max(0, $party->photos_count - count($party->previewPhotos)) }} more</div>
+                                <div class="mb-1">+{{ max(0, $party->photos_count - count($party->previewPhotos)) }}
+                                    more
+                                </div>
                                 <div class="underline">Full gallery</div>
                             </a>
                         </div>
                     </div>
                 </div>
 
-                <div
-                    x-show="showModal"
-                    x-cloak
-                    x-transition
-                    @keydown.window.escape="showModal=false"
-                    class="fixed inset-0 bg-black/75 flex items-center justify-center z-50 px-4"
-                >
-                    <div class="relative max-w-4xl w-full" @click.self="showModal=false">
-                        <button @click="showModal=false"
-                                class="absolute top-3 right-3 text-white bg-black/50 hover:bg-black/70 rounded-full p-3 shadow-lg"
-                                aria-label="Close">&times;</button>
+                <template x-teleport="body">
+                    <div
+                        x-show="showModal"
+                        x-cloak
+                        x-transition
+                        @keydown.window.escape="showModal=false"
+                        class="fixed inset-0 flex items-center justify-center px-4 z-[2147483647]"
+                    >
+                        <!-- Backdrop -->
+                        <div
+                            class="absolute inset-0"
+                            style="background: rgba(0,0,0,0.75);"
+                            @click="showModal=false"
+                        ></div>
 
-                        <button
-                            @click="activeIdx = (activeIdx - 1 + photos.length) % photos.length"
-                            class="absolute left-3 top-1/2 transform -translate-y-1/2 text-white bg-black/50 hover:bg-black/70 rounded-full p-4 shadow-lg"
-                            aria-label="Previous"
-                        >&lsaquo;</button>
+                        <div class="relative z-10 w-full" style="max-width:1200px; height:80vh;" @click.stop>
+                            <!-- Close -->
+                            <button
+                                @click="showModal=false"
+                                style="position:absolute; right:16px; transform:translateY(-50%); z-index:20;"
+                                class="text-white bg-black/60 hover:bg-black/80 rounded-full p-4 shadow-lg"
+                                aria-label="Close"
+                            >&times;</button>
 
-                        <div class="flex justify-center">
-                            <img
-                                :src="`/storage/${photos[activeIdx].path}`"
-                                class="object-contain"
-                                alt="full"
-                                @click.stop
-                                style="max-height: calc(100vh - 80px); max-width: calc(100vw - 180px);"
-                            />
+                            <!-- Prev -->
+                            <button
+                                @click="activeIdx = (activeIdx - 1 + photos.length) % photos.length"
+                                style="position:absolute; left:16px; top:50%; transform:translateY(-50%); z-index:20;"
+                                class="text-white bg-black/60 hover:bg-black/80 rounded-full p-4 shadow-lg"
+                                aria-label="Previous"
+                            >&lsaquo;</button>
+
+                            <!-- Next -->
+                            <button
+                                @click="activeIdx = (activeIdx + 1) % photos.length"
+                                style="position:absolute; right:16px; top:50%; transform:translateY(-50%); z-index:20;"
+                                class="text-white bg-black/60 hover:bg-black/80 rounded-full p-4 shadow-lg"
+                                aria-label="Next"
+                            >&rsaquo;</button>
+
+                            <!-- Image area -->
+                            <div class="w-full h-full flex items-center justify-center select-none" style="padding:2rem 6rem;">
+                                <img
+                                    :src="`/storage/${photos[activeIdx].path}`"
+                                    class="object-contain"
+                                    alt="full"
+                                    style="max-height:100%; max-width:100%;"
+                                />
+                            </div>
                         </div>
-
-                        <button
-                            @click="activeIdx = (activeIdx + 1) % photos.length"
-                            class="absolute right-3 top-1/2 transform -translate-y-1/2 text-white bg-black/50 hover:bg-black/70 rounded-full p-4 shadow-lg"
-                            aria-label="Next"
-                        >&rsaquo;</button>
                     </div>
-                </div>
+                </template>
             </div>
         @endforeach
     </div>
