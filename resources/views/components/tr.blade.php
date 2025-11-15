049cab52acb691bf775480da2575c294e8893964
FlorianTheyerl <theyerl.flo@web.de>
2025-11-15 09:06:55 +0100

    changed: css

--- resources/views/components/tr.blade.php
+++ resources/views/components/tr.blade.php
@@ -1,6 +1,6 @@
 <tr
     {{ $attributes->merge([
-        'class' => 'fi-ta-row fi-ta-summary-row cursor-pointer' . ($selected??0 ? ' !bg-[#f5f5f5] dark:!bg-[#1b232d]' : '')
+        'class' => 'fi-ta-row fi-ta-summary-row cursor-pointer' . ($selected??0 ? ' !bg-[#f5f5f5] dark:!bg-[#212121e6]' : '')
     ]) }}
 >
     {{ $slot }}
